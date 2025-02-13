fc74f03add187befc3fbef06359438eaefcfbca1
Rohit Yakkundi <111986558+rohit9481@users.noreply.github.com>
2025-02-13 17:58:37 +0530

    Add files via upload
    getcode #finding the target in infinite array

---
+++ BinarySearch/getcode.java
@@ -0,0 +1,35 @@
+public class getcode {
+    public static void main(String[] args) {
+        // Infinite Array Simulation
+        int[] arr = {3, 4, 6, 7, 8, 9, 21, 23, 45, 65, 87, 89, 98, 123, 324, 435, 566, 767};
+        int target = 98;
+        System.out.println(ans(arr, target));
+    }
+
+    static int ans(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]) {
+            int newstart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newstart;
+        }
+
+        return binarySearch(arr, target, start, end);
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid; // Fixed: Return index instead of -1
+            }
+        }
+        return -1;
+    }
+}
