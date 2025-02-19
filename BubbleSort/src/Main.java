de8395338e02c65507f2f36a962785684c528c2e
Rohit Yakkundi <111986558+rohit9481@users.noreply.github.com>
2025-02-19 08:22:03 +0530

    Add files via upload

--- BubbleSort/src/Main.java
+++ BubbleSort/src/Main.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
+        int[] arr={3,2,1,4,5};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void bubble(int[] arr){
         boolean swapend;
         for(int i=0;i<arr.length;i++){
-            swapend=false;
+            swapend=false;// if array is already sorted then this will run and goes directly to the last command and break
             for(int j=1;j<arr.length-i;j++){
                 if(arr[j]<arr[j-1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1]=temp;
+                    int temp = arr[j];//2
+                    arr[j] = arr[j-1];//2==1
+                    arr[j-1]=temp;//1==2
                     swapend= true;
                 }
             }
