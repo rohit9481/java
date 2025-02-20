268bc9280418151dfe60f5e7167bbfe54d771ad8
Rohit Yakkundi <111986558+rohit9481@users.noreply.github.com>
2025-02-20 06:30:36 +0530

    Add files via upload

---
+++ BubbleSort/src/insertionsort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class insertionsort {
+    public static void main(String[]args){
+        int[] arr={5,3,4,1,2};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertion(int[] arr){
+        for(int i =0;i<arr.length-1;i++){
+            for(int j =i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+    static void swap(int[] arr,int first, int last){
+        int temp= arr[first];
+        arr[first]= arr[last];
+        arr[last]=temp;
+    }
+}
---
+++ BubbleSort/src/sort_selection.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class sort_selection {
+    public static void main(String[]args){
+        int[] arr={3,1,5,4,2};
+        selectionsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionsort(int[]arr){
+        for(int i=0;i<arr.length;i++){
+            int last=arr.length-i-1;
+            int maxIndex= getMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+    static int getMaxIndex(int[] arr, int start,int last){
+        int max=start;
+        for(int i=start;i<=last;i++){
+            if(arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int start,int last){
+        int temp=arr[start];
+        arr[start]=arr[last];
+        arr[last]=temp;
+    }
+}
